834a3ca202f59b99fdb0568ad64a823c5da8df3c
Jesse Galloway <JessGalloway23@outlook.com>
2022-12-29 14:26:02 -0600

    added some weapons, races, and tested

--- NewDungeon/TestHarness.cs
+++ NewDungeon/TestHarness.cs
@@ -12,9 +12,13 @@ namespace NewDungeon
     {
         static void Main(string[] args)
         {
-
+            Weapon w1 = new Weapon("Kindness","Enemies are friends who haven't yet been understood", 10, 5, true, WeaponTypes.Understanding );
+            
             Character c1 = new Character("Jes" , 50, 50, 100, 100);
             Console.WriteLine(c1);
+
+            Console.WriteLine(w1);
+            Console.WriteLine();
 
 
         }//end Main()
---
+++ NewDungeonClassLibrary/WeaponTypes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewDungeonClassLibrary
+{
+    public enum WeaponTypes
+    {
+
+        Understanding,
+        Authenicity,
+        Honesty,
+        Humility,
+        Respect,
+       
+    }//end enum
+}//end namespace
